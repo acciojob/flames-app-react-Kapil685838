fe87a4ed17132f8be2170ff6730ac9040df13ff7
Kapil Chaudhary <kc685838@gmail.com>
2023-05-12 16:52:31 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Form from './Form.js';
 
-class App extends Component {
-    render() {
+// class App extends Component {
+//     render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+//         return(
+//             <div id="main">
+//                {/* Do not remove the main div */}
+
+//             </div>
+//         )
+//     }
+// }
+
+const App = () => {
+    const [answer, setAnswer] = useState("");
+
+    return(
+        <div id="main">
+           {/* Do not remove the main div */}
+           <Form 
+                setAnswer = {setAnswer}
+           />
+           <h3 data-testid="answer">{answer}</h3>
+        </div>
+    )
 }
 
 

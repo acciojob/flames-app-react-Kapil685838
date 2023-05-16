8e401df16465727b2e48557174d31810c7b6db46
Kapil Chaudhary <kc685838@gmail.com>
2023-05-16 20:43:16 +0530

    submitting code-Tue May 16 2023 20:43:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,6 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 import Form from './Form.js';
 
-// class App extends Component {
-//     render() {
-
-//         return(
-//             <div id="main">
-//                {/* Do not remove the main div */}
-
-//             </div>
-//         )
-//     }
-// }
-
 const App = () => {
     const [answer, setAnswer] = useState("");
 
